a3ce9a48e2703d1de38a57b6207e1397acb3ae9a
Vasko Nikolov <vaskonikolov23@hotmail.com>
2024-12-03 17:01:15 +0200

    Create candidate contacts page

--- src/pages/CandidateContacts.tsx
+++ src/pages/CandidateContacts.tsx
@@ -1,9 +1,9 @@
-const Contacts = () => {
+const CandidateContacts = () => {
   return (
     <div className="flex flex-col justify-center m-5 items-center gap-2">
-      <h1>Contacts</h1>
+      <h1>Candidate Contacts</h1>
     </div>
   );
 };
 
-export { Contacts };
+export { CandidateContacts };
